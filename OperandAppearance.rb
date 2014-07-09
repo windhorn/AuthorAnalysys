731b087bb7a574621f865caec20199031db1d599
DaiFukumoto <wo.horn.dai@gmail.com>
2014-07-09 20:56:01 +0900

    演算子の行あたりの出現回数を取れるようにする．

--- OperandAppearance.rb
+++ OperandAppearance.rb
@@ -2,29 +2,37 @@
 
 resultHash = {}   # 行数とその行で出現した演算子の数を格納するハッシュ
 
-filePath = "/Users/fukumotodai/Desktop/Turtle01_846255.java"
-
+#filePath = "/Users/fukumotodai/Desktop/Turtle01_846255.java"
+filePath = ARGV[0]
 def parseLine(line)
   
-  line = line.gsub(/[0-9]/," ").gsub(/[a-zA-Z]/, " ").gsub(";", " ").gsub(/\{|\}/, " ").gsub(/\(|\)/," ")
+  line = line.gsub(/[0-9]/," ").gsub(/[a-zA-Z]/, " ").gsub(";", " ").gsub(/\{|\}/, " ").gsub(/\(|\)/," ").gsub(".", " ")
   line = line.split(" ")
-  p line
+#  p line
 
   return line
 end
 
 ### ファイルを一行ずつ読み込む
 open(filePath) do |f|
-  f.each do |line|
+
+  f.each_with_index do |line, index|
+    operand = 0
+#    p index
     parseLine(line).each do |item|    # 英数字とか削除した後の各要素
       if item =~ /(\+\+)|(--)|(\+)|(-)|(\*)|(\/)|(%)|(\^)/ && !(item =~ /(\+=)|(-=)|(\*=)|(\/=)|(\^=)/)
-        allOp += 1
+        operand += 1
+#        p "全演算子"
       elsif item =~ /(=)|(\+=)|(-=)|(\*=)|(\/=)|(%=)|(\^=)/ && !(item =~ /(==)/)
-        allOp += 1
-        binOp += 1
+        operand += 1
+#        p "二項代入演算子"
       end
     end
-      p "---------------"    
+    if operand > 0
+      resultHash[index+1] = operand
+    end
+#    p "---------------"    
   end
 end
 
+p resultHash
