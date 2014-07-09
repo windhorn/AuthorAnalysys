eb83f5e1f5c060c9e8b56f578b79151890c4055e
DaiFukumoto <wo.horn.dai@gmail.com>
2014-07-09 23:12:57 +0900

    出力する情報を追加する．
    演算子が出現した行数に対する演算子数の平均
    全行数に対する演算子が出現した行数の割合
    演算子が出現した行数
    全行数

--- OperandAppearance.rb
+++ OperandAppearance.rb
@@ -1,11 +1,12 @@
 ### 演算子の行あたりの出現回数
 
 resultHash = {}   # 行数とその行で出現した演算子の数を格納するハッシュ
-
+lineNum = 0        # ソースコードの全行数
 #filePath = "/Users/fukumotodai/Desktop/Turtle01_846255.java"
 filePath = ARGV[0]
+
+### 受け取った行の英数字,{},(),
 def parseLine(line)
-  
   line = line.gsub(/[0-9]/," ").gsub(/[a-zA-Z]/, " ").gsub(";", " ").gsub(/\{|\}/, " ").gsub(/\(|\)/," ").gsub(".", " ")
   line = line.split(" ")
 #  p line
@@ -17,6 +18,7 @@ end
 open(filePath) do |f|
 
   f.each_with_index do |line, index|
+    lineNum += 1
     operand = 0
 #    p index
     parseLine(line).each do |item|    # 英数字とか削除した後の各要素
@@ -35,4 +37,19 @@ open(filePath) do |f|
   end
 end
 
+### 各行に含まれる演算子の平均を計算する
+opNum = 0
+resultHash.each do |key, value|
+  opNum += value
+end
+
+average = opNum.to_f / resultHash.length.to_f
+p "演算子が出現した行数に対する演算子数の平均: #{average}"
+proportion = resultHash.length.to_f / lineNum.to_f * 100
+p "全行数に対する演算子が出現した行数の割合: #{proportion} %"
+p "演算子が出現した行数: #{resultHash.length}"
+
+
+
+p "全行数: #{lineNum}"
 p resultHash
